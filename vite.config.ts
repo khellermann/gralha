7d8c8b7364c7d278c7d13d4aa450dfe20e4ab501
khellermann <khellermann@gmail.com>
2026-07-09 09:57:30 -0300

    Fix Vercel deployment output

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,5 +9,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [tanstackStart(), nitro({ preset: "vercel" }), react(), tailwindcss(), tsconfigPaths()],
 });
